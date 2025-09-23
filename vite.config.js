c7c52fc71dca04866c0608fdf461ae96c7fc34db
Pradeep Yadav <impradeepy@gmail.com>
2025-09-23 22:46:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: 'src',
+  build: {
+    // This tells Vite to create the 'dist' folder at the project root,
+    // which is what Vercel expects.
+    outDir: '../dist'
+  },
   server: {
     host: true,  // or '0.0.0.0'
     port: 3000,
